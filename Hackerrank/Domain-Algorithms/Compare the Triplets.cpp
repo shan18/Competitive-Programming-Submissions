28e63d6fee9a52e01cf0c49e517ca5d08953aaba
Shantanu Acharya <thegeek.004@gmail.com>
2017-04-30 18:18:21 +0530

    Create Compare the Triplets.cpp

---
+++ Hackerrank/Domain-Algorithms/Compare the Triplets.cpp
@@ -0,0 +1,30 @@
+// GNU C++
+// link: https://www.hackerrank.com/challenges/compare-the-triplets
+
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector < int > solve(int a0, int a1, int a2, int b0, int b1, int b2){
+    // Complete this function
+}
+
+int main() {
+    int a0;
+    int a1;
+    int a2;
+    cin >> a0 >> a1 >> a2;
+    int b0;
+    int b1;
+    int b2;
+    cin >> b0 >> b1 >> b2;
+    vector < int > result = solve(a0, a1, a2, b0, b1, b2);
+    for (ssize_t i = 0; i < result.size(); i++) {
+        cout << result[i] << (i != n - 1 ? " " : "");
+    }
+    cout << endl;
+    
+
+    return 0;
+}

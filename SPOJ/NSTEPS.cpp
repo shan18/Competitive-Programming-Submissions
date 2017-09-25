b00fa4e323e54e097378ac7f6e99d4be3fa7630e
Shantanu Acharya <thegeek.004@gmail.com>
2017-09-25 23:52:00 +0530

    Create NSTEPS.cpp

---
+++ SPOJ/NSTEPS.cpp
@@ -0,0 +1,20 @@
+// C++14
+
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int x, y, n;
+	cin >> n;
+	while(n--) {
+		cin >> x >> y;
+		if (x == y || x - y == 2) {
+			if(x % 2 == 0)
+				cout << x + y << endl;
+			else
+				cout << x + y -1 << endl;
+		} else
+			cout << "No Number" << endl;
+	}
+	return 0;
+}
